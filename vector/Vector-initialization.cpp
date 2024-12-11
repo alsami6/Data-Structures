15d2d5baf104f624df40890781f2e96116b2e24a
alsami6 <alsami1209@gmail.com>
2024-12-11 20:48:49 +0600

    _

---
+++ vector/Vector-initialization.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[5] = {1,2,3,4,5};
+    
+    
+    vector<int> v(a,a+5);
+    
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+
+
+    return 0;
+}
