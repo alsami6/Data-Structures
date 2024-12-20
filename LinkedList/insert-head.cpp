2fb6f234698043a6a63707bb5d027a11257e28de
alsami6 <alsami1209@gmail.com>
2024-12-20 20:24:15 +0600

    _

--- LinkedList/insert-head.cpp
+++ LinkedList/insert-head.cpp
@@ -19,7 +19,7 @@ void insert_at_head(Node* &head,int val){
 }
 
 void print_linked_list(Node* head){
-    Node* tmp = head;
+    Node* tmp = head; 
     while(tmp != NULL){
         cout << tmp->val << " ";
         tmp = tmp->next;
@@ -27,17 +27,19 @@ void print_linked_list(Node* head){
 }
 
 int main(){
-    Node* head = new Node(10);  
+    Node* head = new Node(10);
     Node* a = new Node(20);
-    Node* b = new Node(30);  
-    Node* c = new Node(40);
+    Node* b = new Node(40);
 
     head->next = a;
     a->next = b;
-    b->next = c; 
 
-    insert_at_head(head, 100);
+    insert_at_head(head,100);
+    insert_at_head(head,890);
+    insert_at_head(head,8120);
+
     print_linked_list(head);
+    
 
     return 0;
 }
