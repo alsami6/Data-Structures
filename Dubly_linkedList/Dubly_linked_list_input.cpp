ac53872c70b126ad925561f09820de5a0f2e390a
alsami6 <alsami1209@gmail.com>
2024-12-26 12:07:59 +0600

    Dubly LinkedList

--- Dubly_linkedList/Dubly_linked_list_input.cpp
+++ Dubly_linkedList/Dubly_linked_list_input.cpp
@@ -6,39 +6,39 @@ class Node
     public:
         int val;
         Node* next;
+        Node* prev;
     Node(int val){
         this->val = val;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
-void insert_at_tail(Node* &head, Node* &tail,int val){
+void print_forward(Node* head){
+    Node* tmp = head;
+    while(tmp != NULL){
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
     Node* newnode = new Node(val);
     if(head == NULL){
         head = newnode;
         tail = newnode;
+        return;
     }
+    newnode->prev = tail;
     tail->next = newnode;
-    tail = tail->next;
-}
-
-int sz(Node* head){
-    Node* tmp = head;
-    int cnt = 0;
-
-    while(tmp != NULL){
-        cnt++;
-        tmp = tmp->next;
-    }
-
-    return cnt;
+    tail = newnode;
 }
 
 int main(){
     Node* head = NULL;
     Node* tail = NULL;
-    int val;
 
+    int val;
     while(true){
         cin >> val;
         if(val == -1){
@@ -47,21 +47,7 @@ int main(){
         insert_at_tail(head,tail,val);
     }
 
-    int size = sz(head);
-
-    Node* tmp = head;
-    int index = size / 2;
-
-    for(int i = 1; i < index; i++){
-        tmp = tmp->next;
-    }
-
-    if(size % 2 != 0){
-        cout << tmp->val << endl;
-    }
-    else{
-        cout << tmp->val << " " << tmp->next->val << endl;
-    }
+    print_forward(head);
 
     return 0;
 }
